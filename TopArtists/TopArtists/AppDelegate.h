4ce6ed2e79e04e7bfd8a3d46eff6e8bca52ed1c6
Michael Hartung <hartungm@mail.gvsu.edu>
2014-10-20 12:36:51 -0400

    Initial Xcode Commit

---
+++ TopArtists/TopArtists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopArtists
+//
+//  Created by Michael Hartung on 10/20/14.
+//  Copyright (c) 2014 hartung-michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TopArtists/TopArtists/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  TopArtists
+//
+//  Created by Michael Hartung on 10/20/14.
+//  Copyright (c) 2014 hartung-michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
