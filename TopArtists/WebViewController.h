7fa1834a43eff949a6ad5ca04495eba5374373ef
Michael Hartung <hartungm@mail.gvsu.edu>
2014-10-26 22:34:25 -0400

    Crap changes

---
+++ TopArtists/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  TopArtists
+//
+//  Created by Michael Hartung on 10/26/14.
+//  Copyright (c) 2014 hartung-michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
