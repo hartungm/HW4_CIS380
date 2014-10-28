b54952ddc70a5d67461d07e67f7c8b1643b87220
Michael Hartung <hartungm@mail.gvsu.edu>
2014-10-28 02:29:46 -0400

    Added Things from Lecture

--- TopArtists/TopArtists/DetailViewController.m
+++ TopArtists/TopArtists/DetailViewController.m
@@ -9,7 +9,7 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
-
+@property (strong, nonatomic) UIActivityIndicatorView *webCounter;
 @end
 
 @implementation DetailViewController
@@ -31,17 +31,66 @@
 	if(self.detailItem)
 	{
 		NSString *earlString = self.detailItem[@"url"];
-		NSString *mobileString = [earlString stringByReplacingOccurrencesOfString:@"www" withString:@"m"];
+		NSString *mobileString = [earlString stringByReplacingOccurrencesOfString:@"www.last.fm" withString:@"m.last.fm"];
 		NSURL *earl = [NSURL URLWithString:mobileString];
 		NSURLRequest *request = [NSURLRequest requestWithURL:earl];
+		self.webView.delegate = self;
 		[self.webView loadRequest:request];
 	}
+}
+
+-(UIActivityIndicatorView *) webActivityIndicator
+{
+	if(!self.webCounter)
+	{
+		self.webCounter = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+		self.webCounter.hidesWhenStopped = YES;
+	}
+	return self.webCounter;
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	UIBarButtonItem *activity = [[UIBarButtonItem alloc] initWithCustomView:self.webCounter];
+	UIBarButtonItem *safari = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(viewInSafari)];
+	self.navigationItem.rightBarButtonItems = @[safari, activity];
 	[self configureView];
+}
+
+-(void) updateBackButton
+{
+	if([self.webView canGoBack])
+	{
+		if(!self.navigationItem.leftBarButtonItem)
+		{
+			[self.navigationItem setHidesBackButton:YES animated:YES];
+			UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(backPressed)];
+			[self.navigationItem setLeftBarButtonItem:backItem animated:YES];
+		}
+	}
+	else
+	{
+		[self.navigationItem setLeftBarButtonItem:nil animated:YES];
+		[self.navigationItem setHidesBackButton:NO animated:YES];
+	}
+}
+
+-(void) backPressed
+{
+	if([self.webView canGoBack])
+	{
+		[self.webView goBack];
+	}
+}
+
+-(void)viewInSafari
+{
+	NSURL *earl = [NSURL URLWithString: self.artistURL];
+	if(![[UIApplication sharedApplication] openURL:earl]);
+	{
+		// TODO: Log a message.
+	}
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,4 +98,20 @@
 	// Dispose of any resources that can be recreated.
 }
 
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+	[self.webCounter startAnimating];
+	[self updateBackButton];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+	[self.webCounter stopAnimating];
+	[self updateBackButton];
+}
+
+-(void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+	[self.webCounter stopAnimating];
+}
 @end
